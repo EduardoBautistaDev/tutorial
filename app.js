a169e21d7f0e4c8755d6895e0398fb2dbf95e9be
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-09 22:19:21 -0500

    path

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-const os = require('os')
+const path = require('path');
 
-//info about current user
-const user = os.userInfo()
-console.log(user)
+console.log(path.sep)
 
+const filePath = path.join('/content/', 'subfolder', 'test.txt');
+console.log(filePath);
 
-//system uptime
-console.log(`The system uptime is ${os.uptime()} seconds.`)
-
-const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem()
-}
-
-console.log(currentOS);
