fce7caf0f74fc431574295f2db99903efdb001b8
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-09 22:33:20 -0500

    app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const {readFileSync, writeFileSync} = require('fs');
+const {readFile, writeFile} = require('fs');
 
-const first = readFileSync('./content/first.txt','utf8');
-const second = readFileSync('./content/second.txt', 'utf8');
-
-writeFileSync('./content/result-sync.txt',`Here is the result: ${first}, ${second}`);
+readFile('./content/first.txt', 'utf8',(err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(result)
+})
