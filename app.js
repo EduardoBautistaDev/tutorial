aebf1cca3edb4ce5bc82f0286aa5cc899a365e3c
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-10 21:08:01 -0500

    async

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const {readFile, writeFile, writeFileSync} = require('fs');
-const util = require('util')
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile)
-
+const {readFile, writeFile, writeFileSync} = require('fs').promises;
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 const start = async() => {
     try{
-        const first = await readFilePromise('./content/first.txt', 'utf8')
-        const second = await readFilePromise('./content/second.txt', 'utf8')
-        await writeFilePromise('./content/result-mind-grenade.txt', `This is awesome: ${first}, ${second}`)
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile('./content/result-mind-grenade.txt', `This is awesome: ${first}, ${second}`)
         console.log(first, second)
     }catch(error){
         console.log(error)
