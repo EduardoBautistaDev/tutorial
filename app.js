9d05b916ea9fcd1d74813f3aafba62093c574600
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-10 20:49:53 -0500

    async

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const _ = require('lodash')
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flatMapDeep(items)
-console.log(newItems);
+const {readFile} = require('fs');
+
+const getText= (path) => {
+    return new Promise((resolve, reject)=>{
+        readFile('./content/first.txt', 'utf8', (err, data) => {
+            if (err) {
+                resolve(err)
+            }else {
+                resolve(data)
+            }
+        })
+    })
+}
+
+getText('./content/first.txt').then((result) => console.log(result)).catch((err)=>console.log(err))
